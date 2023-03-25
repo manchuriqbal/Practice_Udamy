b5d39a155bcdd54564e9ddccb6054463a0515e07
manchur <manchur.fiverr@gmail.com>
2023-03-25 17:11:08 +0600

    fainaly i complete this project

--- GeoCoder/app.py
+++ GeoCoder/app.py
@@ -6,7 +6,7 @@ from geopy.geocoders import options
 import os
 
 
-options.default_user_agent = "my-application"
+options.default_user_agent = "app"
 
 app = Flask(__name__)
 
@@ -18,27 +18,33 @@ def index():
 
 @app.route("/succes-table", methods=["POST"])
 def succes_table():
+    global filename
     if request.method == "POST":
         file = request.files["file"]
-        df = pandas.read_csv(file)
-        gc = Nominatim(user_agent="app")
-        df['coordinates'] = df["Address"].apply(gc.geocode)
-        df['Latitude'] = df["coordinates"].apply(
-            lambda x: x.latitude if x != None else None)
-        df['Longitude'] = df["coordinates"].apply(
-            lambda x: x.longitude if x != None else None)
-        df = df.drop("coordinates", axis=1)
-        directory = "uploads"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        df.to_csv("uploads/geocodeed.csv", index=None)
+        try:
+            df = pandas.read_csv(file)
+            gc = Nominatim(user_agent="app")
+            df['coordinates'] = df["Address"].apply(gc.geocode)
+            df['Latitude'] = df["coordinates"].apply(
+                lambda x: x.latitude if x != None else None)
+            df['Longitude'] = df["coordinates"].apply(
+                lambda x: x.longitude if x != None else None)
+            df = df.drop("coordinates", axis=1)
+            directory = "uploads"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            filename = dt.datetime.now().strftime("uploads/%Y-%m-%d-%H-%M-%S-%f" + ".csv")
+            df.to_csv(filename, index=None)
 
-        return render_template("index.html", text=df.to_html(), btn='download.html')
+            return render_template("index.html", text=df.to_html(), btn='download.html')
+
+        except:
+            return render_template("index.html", text="Please make sure you have an address column in your CSV file!")
 
 
 @app.route("/download")
 def download():
-    return send_file("uploads/geocodeed.csv", download_name="yourname.csv", as_attachment=True)
+    return send_file(filename, as_attachment=True, download_name="yourfile.csv")
 
 
 if "__main__" == __name__:
