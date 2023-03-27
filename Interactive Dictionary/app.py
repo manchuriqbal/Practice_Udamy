c080d2100782c3a973e7a7b2167a1908676d1737
manchur <manchur.fiverr@gmail.com>
2023-03-27 16:59:46 +0600

    i add loof, condition, input and more function

---
+++ Interactive Dictionary/app.py
@@ -0,0 +1,32 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+
+def traslate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s insteand? Enter Y if yes. Enter N if no.  " %
+                   get_close_matches(word, data.keys())[0])
+        yn = yn.upper()
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "the word does't exist. please doble check it"
+        else:
+            "we didn't undastand your word. "
+
+    else:
+        return "the word does't exist. please doble check it"
+
+
+word_ = input("Enter a word: ")
+
+output = traslate(word_)
+
+for item in output:
+    print(item)
